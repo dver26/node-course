4a26dfc80ce2c87197b477f71cbb1e7439ca01c6
dver26 <fercorvictor@gmail.com>
2023-08-02 17:57:42 +0200

    added routing and pokemons

---
+++ clase2/routing.js
@@ -0,0 +1,25 @@
+const http = require('node:http')
+
+const dittoJSON = require('./pokemon/ditto.json')
+
+const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
+          res.end('<h1>404 NOT FOUND</h1>')
+      }
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+  console.log('server listening on port http://localhost:1234')
+})
