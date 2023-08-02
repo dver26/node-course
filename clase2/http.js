dd02deb46ca327cbe03aa071c560008adb0c0026
dver26 <fercorvictor@gmail.com>
2023-08-02 17:31:46 +0200

    added sm endpoints

--- clase2/http.js
+++ clase2/http.js
@@ -1,21 +1,29 @@
 const http = require('node:http')
-
-console.log(process.env)
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
   if (req.url === '/') {
     res.statusCode = 200 // OK
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end('Bienvenido a mi página de inicio')
+  } else if (req.url === '/imagen-super-bonita.png') {
+    fs.readFile('./images/luffychikito.jpg', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>Error 500 (INTERNAL SERVER ERROR)</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/jpeg')
+        res.end(data)
+      }
+    })
   } else if (req.url === '/contacto') {
     res.statusCode = 200 // OK
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
     res.end('<h1>Contacto</h1>')
   } else {
     res.statusCode = 404 // NOT FOUND
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
     res.end('<h1>404</h1>')
   }
 }
